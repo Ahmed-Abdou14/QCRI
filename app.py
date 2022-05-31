ff49422c6b15d9aac34e2cf1f2db1122945fbf5d
AhmedAshraf-aa2005605 <104530599+Ahmed-Abdou14@users.noreply.github.com>
2022-06-01 00:49:18 +0300

    + Repo file
    Created a repo file that creates the table if needed and populates it from fetching GitHub API.
    app.py calls other methods in repo.py to fetch from psql

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import requests, json
+import json
 import repo
 
 app = Flask(__name__)
@@ -7,26 +7,20 @@ psql_repo = repo.Repo()
 
 @app.route('/')
 def index():
-    return f'<a href="/topRepos">peak top github repos{psql_repo.get_top_repos()}</a>'
+    return f'<a href="/topRepos">peak top github repos #{psql_repo.get_repo_count()}</a>'
 
 base_url = 'https://api.github.com'
 @app.route('/topRepos')
 def get_top_repos():
-    url = f'{base_url}/search/repositories?q=stars:>0&sort=stars&order=desc'
-    limit = request.args.get("repo_count", type=int, default=3)
-    response = requests.get(url).json()
-    top_repos = response['items'][0:limit]
-    return jsonify(list(
-        map(
-            lambda repo: 
-                {
-                    "repo_name": repo['name'], 
-                    "repo_url": repo['html_url'],
-                    "star_count": repo['stargazers_count'],
-                    "forks_count": repo['forks_count']
-                }
-        , top_repos)
-    ))
+    limit = request.args.get("repo_count", type=int, default=10)
+    top_repos = psql_repo.get_top_repos(limit)
+    return jsonify(
+        [
+            dict(
+                (['repo_name', 'repo_url', 'star_count', 'forks_count'][i], value) for i, value in enumerate(row)
+            ) for row in top_repos
+        ]
+    )
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- repo.py
+++ repo.py
@@ -1,8 +1,9 @@
+import requests
 from psycopg2 import connect
-
 class Repo:
 
     def __init__(self):
+        #connecting to psql
         self.__db_name = 'postgres'
         self.__conn = connect(
             dbname = self.__db_name,
@@ -12,14 +13,61 @@ class Repo:
         )
         self.__cursor = self.__conn.cursor()
         
-    def get_top_repos(self):
+        #creating table
+        self.__create_top_repos_table()
+        
+    def get_top_repos(self, count=10):
+        #getting x top repos
+        self.__cursor.execute(f"SELECT * FROM TOP_REPOS LIMIT {count};")
+        return self.__cursor.fetchall()
+    
+    def get_repo_count(self):
+        self.__cursor.execute(f"SELECT COUNT(*) FROM TOP_REPOS;")
+        return self.__cursor.fetchone()
+    
+    def __create_top_repos_table(self):
+        #creates relation if it doesn't exist
+        query = '''
+            CREATE TABLE IF NOT EXISTS TOP_REPOS (
+                repo_name  VARCHAR ( 60 ) PRIMARY KEY,
+                repo_url VARCHAR ( 1000 ) NOT NULL,
+                star_count INT NOT NULL,
+                forks_count INT NOT NULL
+            );
+        '''
+        self.__cursor.execute(query)
+        
+        #populates table from github api if table is empty
+        if self.__is_empty_table('TOP_REPOS'):            
+            self.__populate_top_repos()
+            
+        self.__conn.commit()
+        
+    def __is_empty_table(self, tabel_name):
+        self.__cursor.execute(f"SELECT COUNT(*) FROM (SELECT 1 FROM {tabel_name} LIMIT 1) AS t;")
+        return not self.__cursor.fetchone()[0]
+    
+    def __populate_top_repos(self):
+        url = 'https://api.github.com/search/repositories?q=stars:>0&sort=stars&order=desc'
+        response = requests.get(url).json()['items']
+        
+        #turning all repos into tuples then to strings
+        values_tuples = list(
+            map(
+                lambda repo: str(
+                    (
+                        repo['name'], 
+                        repo['html_url'],
+                        repo['stargazers_count'],
+                        repo['forks_count']
+                    )
+                )
+            , response)
+        )
+        #separating each value tuple with a ','
+        values = ','.join(values_tuples)
         insert_query = f'''
             INSERT INTO TOP_REPOS(repo_name, repo_url, star_count, forks_count)
-            VALUES
-                ('3', '3', 3, 3),
-                ('2', '2', 2, 2);
+            VALUES {values}
         '''
-        #self.__cursor.execute(insert_query)
-        self.__cursor.execute(f"SELECT COUNT(*) FROM top_repos;")
-        return self.__cursor.fetchone()
-        
+        self.__cursor.execute(insert_query)
